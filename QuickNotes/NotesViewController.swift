2021590391cdeb1712cc00978b379261d01fd3e8
patelpra337 <patelpra337@gmail.com>
2019-05-06 14:37:27 -0500

    Create View Controller & Setup Linkage

---
+++ QuickNotes/NotesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotesViewController.swift
+//  QuickNotes
+//
+//  Created by patelpra on 5/6/19.
+//  Copyright © 2019 Pravin Patel. All rights reserved.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
